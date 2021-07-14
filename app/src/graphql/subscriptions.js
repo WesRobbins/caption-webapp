935d3947025f64951a8cd9a13a050662130faf07
Wes <wrbigsky@gmail.com>
2021-07-14 10:56:04 -0600

    add graphql

---
+++ app/src/graphql/subscriptions.js
@@ -0,0 +1,45 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = /* GraphQL */ `
+  subscription OnCreateTodo {
+    onCreateTodo {
+      id
+      name
+      cap1
+      cap2
+      cap3
+      image
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateTodo = /* GraphQL */ `
+  subscription OnUpdateTodo {
+    onUpdateTodo {
+      id
+      name
+      cap1
+      cap2
+      cap3
+      image
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteTodo = /* GraphQL */ `
+  subscription OnDeleteTodo {
+    onDeleteTodo {
+      id
+      name
+      cap1
+      cap2
+      cap3
+      image
+      createdAt
+      updatedAt
+    }
+  }
+`;
